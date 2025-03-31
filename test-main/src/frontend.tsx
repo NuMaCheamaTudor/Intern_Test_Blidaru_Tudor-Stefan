e30f92f9804e0c6af131b8b6cb8c695c4b4d38ec
NuMaCheamaTudor <tudorblidaru@gmail.com>
2025-03-31 17:20:46 +0300

    .

--- test-main/src/frontend.tsx
+++ test-main/src/frontend.tsx
@@ -5,16 +5,22 @@
  * It is included in `src/index.html`.
  */
 
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import {createRoot} from "react-dom/client";
+import {App} from "./App";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "@/dev";
 
 function start() {
-	const root = createRoot(document.getElementById("root")!);
-	root.render(<App />);
+    const root = createRoot(document.getElementById("root")!);
+    root.render(<DevSupport ComponentPreviews={ComponentPreviews}
+                            useInitialHook={useInitial}
+    >
+        <App/>
+    </DevSupport>);
 }
 
 if (document.readyState === "loading") {
-	document.addEventListener("DOMContentLoaded", start);
+    document.addEventListener("DOMContentLoaded", start);
 } else {
-	start();
+    start();
 }
